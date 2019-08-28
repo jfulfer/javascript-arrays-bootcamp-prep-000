14fc56c3187530032cb58fb1fa1135892cd5d906
Jeff Fulfer <jfulfer3@gmail.com>
2019-08-28 14:39:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(chocolateBars, newBar) {
-  var newArray = [...chocolateBars, "Almond Joy"];
+  var newArray = ["Almond Joy", ...chocolateBars];
   return newArray;
 }
