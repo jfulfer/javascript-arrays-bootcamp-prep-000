8fde63834478fd71e7fca312f61f0e686b519d07
Jeff Fulfer <jfulfer3@gmail.com>
2019-08-28 14:35:51 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(["Red", "Blue", "Green"], newColor) {
+function addElementToBeginningOfArray(chocolateBars, newBar) {
   
 }
